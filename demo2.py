7edccadea7b2963d91cc1f30c464b68dd8c3f577
hainhe <124561362+hainhe@users.noreply.github.com>
2025-01-14 11:35:03 +0700

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -1,18 +1,10 @@
 from flask import Flask, request
 import requests
-from datetime import datetime
-import pytz  # Thư viện để xử lý múi giờ
 
 app = Flask(__name__)
 
 TELEGRAM_BOT_TOKEN = '7637391486:AAEYarDrhPKUkWzsoteS3yiVgB5QeiZdKoI'
 CHAT_ID = '-4708928215'
-
-# Hàm lấy thời gian hiện tại theo múi giờ UTC+7
-def get_current_time():
-    utc_plus_7 = pytz.timezone('Asia/Bangkok')  # UTC+7 tương ứng với Asia/Bangkok
-    current_time = datetime.now(utc_plus_7)
-    return current_time.strftime('%Y-%m-%d %H:%M:%S')  # Định dạng: Năm-Tháng-Ngày Giờ:Phút:Giây
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -27,13 +19,7 @@ def webhook():
         print("Error parsing JSON:", str(e))
         return "Invalid JSON", 400
 
-    # Lấy thời gian hiện tại
-    current_time = get_current_time()
-
-    # Thêm thời gian vào thông báo gửi đi
-    message_with_time = f"{message}\n\nTime: {current_time} (UTC+7)"
-
-    send_message_to_telegram(message_with_time)
+    send_message_to_telegram(message)
     return "Webhook received", 200
 
 def send_message_to_telegram(message):
